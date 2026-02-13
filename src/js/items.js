9cf8fa5e512cb26de97297b74086b3787e25b01d
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2026-02-13 17:28:13 +0200

    add put

--- src/js/items.js
+++ src/js/items.js
@@ -73,8 +73,181 @@ const getItemById = async (event) => {
     return;
   }
 
+  // 1. kehittäjälle virheet ja onnistumiset
   console.log(item);
+  // 2. Käyttäjälle tieto näkyviin ja mahdolliset virheet näyviin
   alert(`Item found :) ${item.name}`);
 };
 
-export { getItems, getItemById };
+// DELETE item by ID
+/////////////////////
+
+const deleteItemById = async () => {
+  console.log('Deletoidaan IDn avulla!!!');
+
+  const idInput = document.querySelector('#itemId');
+  const itemId = idInput.value;
+  console.log(itemId);
+
+  // Muista usein tarkistaa että käyttäjä lähettää oikean datan
+  if (!itemId) {
+    console.log('Item ID missing, fill in the details!!!!');
+    return;
+  }
+
+  const confirmed = confirm(
+    `Oletko varma että haluat poistaa itemin: ${itemId}`
+  );
+  // Jos käyttäjä painaa cancel niin palautuu FALSE ja hypätään pois
+  if (!confirmed) return;
+
+  const url = `http://localhost:3000/api/items/${itemId}`;
+
+  const options = {
+    method: 'DELETE',
+  };
+
+  const item = await fetchData(url, options);
+
+  if (item.error) {
+    console.log(item.error);
+    return;
+  }
+
+  console.log(item);
+  alert(item.message);
+
+  // Päivitä UI niin käyttäjä tietää että hedelmä poistui
+  await getItems();
+};
+
+// POST item
+/////////////////////
+
+const addItem = async (event) => {
+  event.preventDefault();
+
+  const form = document.querySelector('add-item-form');
+  const fruitName = document.querySelector('#newItemName').value.trim();
+  const fruitWeight = document.querySelector('#newItemWeight').value.trim();
+
+  if (!fruitName) {
+    alert('Nimi puuttuu!!!!!!');
+    return;
+  }
+
+  const body = {};
+
+  const url = `http://localhost:3000/api/items`;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: fruitName,
+      weight: fruitWeight,
+    }),
+  };
+
+  console.log(url, options);
+
+  const response = await fetchData(url, options);
+
+  if (response.error) {
+    console.log(response.error);
+    return;
+  }
+
+  await getItems();
+  console.log(response);
+};
+
+// Load item to PUT form (GET by id -> fill inputs)
+/////////////////////////////
+
+const loadItemToPutForm = async () => {
+  const idInput = document.querySelector('#putItemId');
+  const nameInput = document.querySelector('#putItemName');
+
+  const itemId = idInput.value.trim();
+
+  if (!itemId) {
+    alert('Anna Item ID');
+    return;
+  }
+
+  const url = `http://localhost:3000/api/items/${itemId}`;
+
+  const item = await fetchData(url);
+
+  if (item.error) {
+    console.error(item.error);
+    alert(`Error: ${item.error}`);
+    return;
+  }
+
+  // täytetään nimi kenttään
+  // mikäli nimeä ei saada haettua niin käytä tyhjää merkkijonoa
+  nameInput.value = item.name ?? '';
+
+  alert(`Haettu item: ${item.name}`);
+};
+
+// Update item (PUT by id)
+/////////////////////////////
+
+const updateItemById = async (event) => {
+  event.preventDefault();
+
+  const idInput = document.querySelector('#putItemId');
+  const nameInput = document.querySelector('#putItemName');
+
+  const itemId = idInput.value.trim();
+  const newName = nameInput.value.trim();
+
+  if (!itemId) {
+    alert('Item ID puuttuu');
+    return;
+  }
+
+  if (!newName) {
+    alert('Uusi nimi puuttuu');
+    return;
+  }
+
+  const url = `http://localhost:3000/api/items/${itemId}`;
+
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: newName }),
+  };
+
+  const updated = await fetchData(url, options);
+
+  if (updated.error) {
+    console.error(updated.error);
+    alert(`Error: ${updated.error}`);
+    return;
+  }
+  console.log(updated);
+  console.log(updated.message);
+  alert(`Item updated: ${updated.item.name}`);
+
+  // valinnainen: päivitä lista uudestaan
+  // await getItems();
+
+  idInput.value = '';
+  nameInput.value = '';
+};
+
+export {
+  getItems,
+  getItemById,
+  deleteItemById,
+  addItem,
+  loadItemToPutForm,
+  updateItemById,
+};
