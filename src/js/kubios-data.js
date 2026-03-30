510368c70ba7c6abc59f12ab9bc04e258f87c18a
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2026-03-30 11:24:34 +0300

    add formatKubiosResults

--- src/js/kubios-data.js
+++ src/js/kubios-data.js
@@ -42,15 +42,43 @@ const getUserData = async () => {
 
   // Draw chart with chart.js
   drawChart(userData);
+  let formattedData = formatKubiosResults(userData);
+  console.log('Formatted Data', formattedData);
   // Draw chart with amcharts
-  // drawAMChart(formattedData);
+  drawAMChart(formattedData);
 };
 
 // You need to formulate data into correct structure in the BE
 // OR you can extract the data here in FE from one or multiple sources
 // Extract data: https://www.w3schools.com/jsref/jsref_map.asp
 
-const formatKubiosResults = (userData) => {};
+const formatKubiosResults = (userData) => {
+  // Formatter chat.js varten
+  const formatter = new Intl.DateTimeFormat('fi-FI', {
+    day: 'numeric',
+    month: 'long',
+  });
+
+  const formattedData = userData.results.map((entry) => {
+    // Muunnetaan päivämäärä Date-olioksi
+    const dateObject = new Date(entry.daily_result);
+    // Muotoillaan label (esim "19. elokuuta") chart.js varten
+    const formattedLabel = formatter.format(dateObject);
+    // Timestamp amCharts varten, muuttaa päivämäärän numeroksi
+    const timestamp = dateObject.getTime();
+
+    // palautetaan muotoiltu objekti jossa oikeat arvot
+    return {
+      date: entry.daily_result, // alkuperäine päivämäätä
+      timestamp: timestamp, // amcharts
+      label: formattedLabel,
+      readiness: entry.result.readiness,
+      stressIndex: entry.result.stress_index,
+    };
+  });
+
+  return formattedData;
+};
 
 // Let us try these together
 const drawChart = (userData) => {
@@ -130,6 +158,95 @@ const drawAMChart = () => {
   // https://www.amcharts.com/demos/line-graph/
   // Documentation
   // https://www.amcharts.com/docs/v5/getting-started/
+  am5.ready(function () {
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new('chartdiv');
+
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: 'panX',
+        wheelY: 'zoomX',
+        pinchZoomX: true,
+        paddingLeft: 0,
+      })
+    );
+
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set(
+      'cursor',
+      am5xy.XYCursor.new(root, {
+        behavior: 'none',
+      })
+    );
+    cursor.lineY.set('visible', false);
+
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xAxis = chart.xAxes.push(
+      am5xy.DateAxis.new(root, {
+        maxDeviation: 0.2,
+        baseInterval: {
+          timeUnit: 'day',
+          count: 1,
+        },
+        renderer: am5xy.AxisRendererX.new(root, {
+          minorGridEnabled: true,
+        }),
+        tooltip: am5.Tooltip.new(root, {}),
+      })
+    );
+
+    var yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {
+          pan: 'zoom',
+        }),
+      })
+    );
+
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(
+      am5xy.LineSeries.new(root, {
+        name: 'Readiness',
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: 'value',
+        valueXField: 'date',
+        tooltip: am5.Tooltip.new(root, {
+          labelText: '{valueY}',
+        }),
+      })
+    );
+
+    // Add scrollbar
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+    chart.set(
+      'scrollbarX',
+      am5.Scrollbar.new(root, {
+        orientation: 'horizontal',
+      })
+    );
+
+    // Set data
+    var data = generateDatas(1200);
+    series.data.setAll(data);
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, 100);
+  }); // end am5.ready()
 };
 
 export { getUserData, getUserInfo };
